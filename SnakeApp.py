8d71271bffd7f4d2de79df03b87e295685027928
jonortega <jon.ortega.g@gmail.com>
2020-12-05 20:56:35 +0100

    Simple debugging

--- SnakeApp.py
+++ SnakeApp.py
@@ -3,11 +3,13 @@
 # class Apple:
 
 class Game:
-	def __init__(heigth, width):
+	def __init__(self, heigth, width):
 		self.heigth = heigth
 		self.width = width
 
-	def render():
-		print("Heigth: " + self.heigth)
-		print("Width: " + self.width)
+	def render(self):
+		print("Heigth: " + str(self.heigth))
+		print("Width: " + str(self.width))
 
+game = Game(10, 20)
+game.render()
