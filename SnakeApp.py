2725d7a86927957990e1ed1126757e4c71bcfe9b
jonortega <jon.ortega.g@gmail.com>
2020-12-05 20:47:05 +0100

    Created base classes

---
+++ SnakeApp.py
@@ -0,0 +1,13 @@
+# class Snake:
+
+# class Apple:
+
+class Game:
+	def __init__(heigth, width):
+		self.heigth = heigth
+		self.width = width
+
+	def render():
+		print("Heigth: " + self.heigth)
+		print("Width: " + self.width)
+
